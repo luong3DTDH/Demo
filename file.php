6141aefec1b893eb11ac9bd8bbf0786189238a21
luong3DTDH <65522901+luong3DTDH@users.noreply.github.com>
2020-05-20 10:37:52 +0700

    file.php

---
+++ file.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <form>
+ <label for="name">Name:</label><br>
+ <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngfuel.com%2Ffree-png%2Frbggq&psig=AOvVaw2zPHUFpUTI-67qKQ4kOFkA&ust=1590032026464000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNiI04bBwekCFQAAAAAdAAAAABAD" alt="avatar">
+ <input type="text" id="fb" name="fb">
+ </form>
+ <body>
+ <p>Hello World</p>
+ </body>
+</html>
